f06ffe349ee8174a4939f53e7a0a67426258b34b
unknown <affankhan627@gmail.com>
2019-03-08 02:29:20 +0500

    php-curl

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -89,8 +89,7 @@ class WalletController extends Controller
         $passphrase = str_random(10);
 
         $adapter = getBlockchainAdapter($coin);
-        echo($adapter->getFamily);
-
+        
         $data = $adapter->generateWallet($label, $passphrase,$user->id,$user->name);
 
         $wallet = $user->getCoinWallet($coin)->create([

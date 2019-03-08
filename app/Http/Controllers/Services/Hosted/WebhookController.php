451f3645de0fab6c539634cbaae4c416e2d2fdbf
unknown <affankhan627@gmail.com>
2019-03-09 00:30:40 +0500

    php-curl

---
+++ app/Http/Controllers/Services/Hosted/WebhookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Services\Hosted;
+
+use App\Jobs\Transactions\ProcessBitcoin;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
+
+class WebhookController extends Controller
+{
+    /**
+     * @param Request $request
+     */
+    public function handleBitcoin(Request $request)
+    {
+        if ($request->type == 'transfer') {
+            ProcessBitcoin::dispatch($request->all());
+        }
+    }
+}

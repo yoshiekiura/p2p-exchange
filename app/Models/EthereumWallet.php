9e99ed36d28e04a6c330c2a42485efc73577af9c
unknown <affankhan627@gmail.com>
2019-03-31 03:43:15 +0500

    eth

--- app/Models/EthereumWallet.php
+++ app/Models/EthereumWallet.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class EthereumWallet extends Model
 {
-    //
+	use WalletSecurityLayer;
+
+	/**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get wallet addresses
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function addresses()
+    {
+        return $this->hasMany('App\Models\EthereumAddress', 'wallet_id', 'id');
+    }
+
+    /**
+     * Get wallet transactions
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactions()
+    {
+        return $this->hasMany('App\Models\EthereumTransaction', 'wallet_id', 'id');
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
 }

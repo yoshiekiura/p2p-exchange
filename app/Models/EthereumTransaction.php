d4562910c821a1d671891c02d9de1203b5d29c8f
unknown <affankhan627@gmail.com>
2019-03-31 03:46:14 +0500

    eth

--- app/Models/EthereumTransaction.php
+++ app/Models/EthereumTransaction.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class EthereumTransaction extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+
+    /**
+     * Get user wallet
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function wallet()
+    {
+        return $this->belongsTo('App\Models\EthereumWallet', 'wallet_id', 'id');
+    }
+
 }

0e2ad764f0feff63efe314f9784c2443dced3f0b
unknown <affankhan627@gmail.com>
2019-03-31 03:48:20 +0500

    eth

--- app/Models/EthereumAddress.php
+++ app/Models/EthereumAddress.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class EthereumAddress extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get user wallet
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function wallet()
+    {
+        return $this->belongsTo('App\Models\EthereumWallet', 'wallet_id', 'id');
+    }
+
 }

5d6ce8e725a02569283c75d818efa958c44d7bca
unknown <affankhan627@gmail.com>
2019-04-01 19:26:02 +0500

    earn

--- resources/views/admin/earnings/partials/tabs/komodo.blade.php
+++ resources/views/admin/earnings/partials/tabs/komodo.blade.php
@@ -3,24 +3,24 @@
         <div class="card-body bg-warning cc KMD pb-1">
             <div class="row text-white">
                 <div class="col-6">
-                    <i class="cc BTC-alt font-large-1" title="BTC"></i>
+                    <i class="cc KMD-alt font-large-1" title="KMD"></i>
                     <h4 class="pt-1 m-0 text-white">
-                        {{$escrow_wallet->get('btc')['total']}} BTC
+                        {{$escrow_wallet->get('kmd')['total']}} KMD
                     </h4>
                 </div>
                 <div class="col-6 text-right pl-0">
                     <h2 class="text-white mb-2 font-large-1">
-                        {{$escrow_wallet->get('btc')['price']}}
+                        {{$escrow_wallet->get('kmd')['price']}}
                     </h2>
-                    <h4 class="text-white">Bitcoin</h4>
+                    <h4 class="text-white">Komodo</h4>
                 </div>
             </div>
         </div>
 
         <div class="card-body">
-            @if(getEscrowWallet('btc')->count())
+            @if(getEscrowWallet('kmd')->count())
                 <div class="table-responsive my-1">
-                    <table id="bitcoin-wallets" class="table table-borderless table-striped row-grouping display icheck table-middle">
+                    <table id="komodo-wallets" class="table table-borderless table-striped row-grouping display icheck table-middle">
                         <thead>
                         <tr>
                             <th></th>

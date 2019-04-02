9739d15bbbdf64eb9c0441e1a8edf56fd0e8e7a0
unknown <affankhan627@gmail.com>
2019-04-02 06:10:28 +0500

    earn

--- config/settings.php
+++ config/settings.php
@@ -16,7 +16,7 @@ return [
         'locked_balance' => env('SET_BTC_LOCKED_BALANCE'),
         'profit_per_wallet_limit' => env('SET_BTC_PROFIT_PER_WALLET_LIMIT', 100000000),
         'trade_fee' => env('SET_BTC_TRADE_FEE', 1),
-        'market_price_percent' => env('SET_BTC_MARKET_CENT', 0)
+        'market_price_percent' => env('SET_BTC_MARKET_CENT', 0),
         'dust_threshold' => 0.00003,
         'min_tx_confirmations'=>3,
     ],
@@ -25,6 +25,7 @@ return [
         'locked_balance' => env('SET_KMD_LOCKED_BALANCE'),
         'profit_per_wallet_limit' => env('SET_KMD_PROFIT_PER_WALLET_LIMIT', 100000000),
         'trade_fee' => env('SET_KMD_TRADE_FEE', 1),
+        'market_price_percent' => env('SET_KMD_MARKET_CENT', 0),
         'dust_threshold' => 0.00003,
         'min_tx_confirmations'=>3,
     ],

e04142c02572459da4907076cdd1ef11fb12f4a9
unknown <affankhan627@gmail.com>
2019-04-02 05:58:19 +0500

    earn

--- resources/views/admin/earnings/partials/tabs/settings.blade.php
+++ resources/views/admin/earnings/partials/tabs/settings.blade.php
@@ -24,6 +24,20 @@
                             </div>
                         </div>
 
+                        <div class="bs-callout-success callout-transparent callout-border-left mb-2 p-1">
+                            <p class="card-text">
+                                {{__('Set the percentage to alter the market price')}}
+                            </p>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('SET_BTC_MARKET_CENT', __('Bitcoin MarketPrice %'), ['class' => 'col-md-3']) !!}
+                            <div class="col-md-9">
+                                {!! Form::text('SET_BTC_MARKET_CENT', $env['SET_BTC_MARKET_CENT']['value'], ['class' => 'form-control', 'required']) !!}
+                            </div>
+                        </div>
+
+
                     </div>
 
                     <div class="form-actions right">
